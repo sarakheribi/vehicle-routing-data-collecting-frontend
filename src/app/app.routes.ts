5d5eb387c53133ad474d066785fb1b9591596841
SaraKheribi <sara_kheribi@hotmail.com>
2024-05-12 16:38:47 +0200

    "technischer Durchstich"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {AddVehicleComponent} from "./pages/add-vehicle/add-vehicle.component";
+import {MapComponent} from "./map/map.component";
 
 export const routes: Routes = [
   { path: '', component: AddVehicleComponent },
+  { path: 'map', component: MapComponent }
 
 ];
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as L from 'leaflet';
 import 'leaflet-routing-machine';
 import 'leaflet/dist/leaflet.css'
 
@@ -11,5 +11,21 @@ import 'leaflet/dist/leaflet.css'
   styleUrl: './map.component.css'
 })
 export class MapComponent {
+  private map: L.Map | undefined;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+
 
 }
