57592e1783afa35fe198a823d0b973ff7789f95c
SaraKheribi <sara_kheribi@hotmail.com>
2024-06-07 23:19:29 +0200

    log in

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,41 @@
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {KeycloakBearerInterceptor, KeycloakService} from "keycloak-angular";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:9090',
+        realm: 'routingapp',
+        clientId: 'routing-frontend',
+      },
+      initOptions: {
+        onLoad: 'login-required',
+      },
+      enableBearerInterceptor: true,
+      loadUserProfileAtStartUp: true
+    });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(),provideHttpClient()]
+  providers: [
+    KeycloakService,
+    {
+      provide: APP_INITIALIZER, useFactory: initializeKeycloak,
+      deps: [KeycloakService],
+      multi: true
+    },
+    {provide: HTTP_INTERCEPTORS, useClass: KeycloakBearerInterceptor, multi: true},
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes), provideClientHydration(), provideAnimationsAsync()
+
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {AddVehicleComponent} from "./pages/add-vehicle/add-vehicle.component";
 import {MapComponent} from "./map/map.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {VehiclesComponent} from "./pages/vehicles/vehicles.component";
 
 export const routes: Routes = [
-  { path: '', component: AddVehicleComponent },
-  { path: 'map', component: MapComponent }
-
+  {path: '', component: HomeComponent},
+  {path: 'vehicles', component: VehiclesComponent},
+  {path: 'map', component: MapComponent}
 ];
